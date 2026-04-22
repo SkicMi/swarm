6812b22801e9a514bc18daa3d4ef4b7a419b4832
SkicMi <danijelmiskic99@gmail.com>
2026-04-22 12:55:28 +0200

    feat: add orchestrator module files

--- src/orchestrator/__init__.py
+++ src/orchestrator/__init__.py
@@ -1,23 +1,21 @@
-from .orchestrator import SwarmOrchestrator, OrchestratorConfig, ModelType, get_model_for_task, get_orchestrator_model
-from .task import Task, TaskPriority, TaskResult
-from .agent import AgentConfig, AgentResult, AgentStatus
-from .worker_pool import WorkerPool, WorkerPoolConfig
-from .task_queue import TaskQueue, TaskQueueConfig
+from .orchestrator import (
+    SwarmOrchestrator,
+    SwarmOrchestratorWithHealing,
+    OrchestratorConfig,
+    ModelType,
+    get_model_for_task,
+    get_orchestrator_model,
+    SelfHealer,
+    HealingMetrics,
+)
 
 __all__ = [
     "SwarmOrchestrator",
+    "SwarmOrchestratorWithHealing",
     "OrchestratorConfig",
     "ModelType",
     "get_model_for_task",
     "get_orchestrator_model",
-    "Task",
-    "TaskPriority",
-    "TaskResult",
-    "AgentConfig",
-    "AgentResult",
-    "AgentStatus",
-    "WorkerPool",
-    "WorkerPoolConfig",
-    "TaskQueue",
-    "TaskQueueConfig",
+    "SelfHealer",
+    "HealingMetrics",
 ]
